07bd18198e977a1c3e811ad43ff15266bcfffadf
diegorllq <rosalesdiego008@gmail.com>
2026-07-02 14:54:16 -0600

    react9

---
+++ practica9react/mi-primer-react/src/MiComponente.jsx
@@ -0,0 +1,10 @@
+export default function MiComponente({titulo, lugar}) {
+    return  (
+        <section className="card">
+            <h2 className="card-tittle">{Titulo}</h2>
+            <h3 className="card-subtittle">{lugar}</h3>
+        </section>
+    )
+                
+    
+}
